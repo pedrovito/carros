4dd78c5dc9bee42a3bc3b2096f32ccf8984a5d22
pedrovito <pedro.v.oliveira007@gmail.com>
2024-03-18 07:40:40 -0300

    quinto commit do app

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import sedans from "./assets/icon-sedans.svg"
 
 export default function App(){
   return(
-    <main>
+    <main className="min-h-svh bg-offwhitebg py-20 px-6">
       <section>
         <div className="bg-orange">
           <img src={sedans} alt="" />
