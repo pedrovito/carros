7fc868f474ca9b52d82d14be88b258cc8d6a5fe4
pedrovito <pedro.v.oliveira007@gmail.com>
2024-03-18 07:31:08 -0300

    teceiro comito do app

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,21 @@ export default function App(){
   return(
     <main>
       <section>
-        <div>
+        <div className="bg-orange">
           <img src={sedans} alt="" />
-          <h2 className="text-3xl">Sedans</h2>
+          <h2 >Sedans</h2>
           <p>
             Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.
           </p>
           <a href="https://www.webmotors.com.br/sedans" target="_blank">ver mais</a>
         </div>
-        <div>
+        <div className="bg-green">
           <img src={suvs} alt="" />
           <h2>SUVs</h2>
           <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
           <a href="https://www.webmotors.com.br/SUVs" target="_blank">ver mais</a>
         </div>
-        <div>
+        <div className="bg-dark-green">
           <img src={luxury} alt="" />
           <h2>Luxuosos</h2>
           <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors:{
+      'orange':'#e28625',
+      'green': '#006971',
+      'dark-green':'  #004140',
+      'offwhite': 'rgba(255, 255, 255, 0.75)',
+      'white': '#fff',
+      'offwhitebg': '#f2f2f2',
+    }
   },
   plugins: [],
 }
