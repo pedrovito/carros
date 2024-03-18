13f904a492073846ad72dd9a0b4cbd00925112b3
pedrovito <pedro.v.oliveira007@gmail.com>
2024-03-18 08:50:51 -0300

    arrumando errinho

--- src/components/Luxury.jsx
+++ src/components/Luxury.jsx
@@ -9,8 +9,8 @@ export default function Luxury() {
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg antialiased" >Luxuosos</h2>
 
           <p className="font-lexend mb-7 text-offwhite lg:mb-20" >Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
-          
-          <a className="font-lexend bg-offwhite  block px-10 py-3 w-fit rounded-3xl text-dark-green border-2 border-offwhitebg border-2 border-offwhitebg hover:bg-dark-green hover:text-white" href="https://www.webmotors.com.br/carros-de-luxo/carros/rs-porto-alegre?estadocidade=Rio%20Grande%20do%20Sul%20-%20Porto%20Alegre&necessidade=Carros%20de%20luxo&idcmpint=t1:c17:m07:busque-por-categorias::carros-de-luxo:comprar" target="_blank">ver mais</a>
+
+          <a className="font-lexend bg-offwhite  block px-10 py-3 w-fit rounded-3xl text-orange border-2 border-offwhitebg hover:bg-dark-green hover:text-white" href="https://www.webmotors.com.br/carros-de-luxo/carros/rs-porto-alegre?estadocidade=Rio%20Grande%20do%20Sul%20-%20Porto%20Alegre&necessidade=Carros%20de%20luxo&idcmpint=t1:c17:m07:busque-por-categorias::carros-de-luxo:comprar" target="_blank">ver mais</a>
 
     </section>
   )
