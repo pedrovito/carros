dadb3825281cd08bd55828ca7e3cb8051c933cec
pedrovito <pedro.v.oliveira007@gmail.com>
2024-03-18 07:04:32 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import luxury from "./assets/icon-luxury.svg"
+import suvs from "./assets/icon-suvs.svg"
+import sedans from "./assets/icon-sedans.svg"
+
 export default function App(){
   return(
-    <h1>Hello pedro</h1>
+    <main>
+      <section>
+        <div>
+          <img src={sedans} alt="" />
+          <h2>Sedans</h2>
+          <p>
+            Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.
+          </p>
+          <a href="https://www.webmotors.com.br/sedans" target="_blank">ver mais</a>
+        </div>
+        <div>
+          <img src={suvs} alt="" />
+          <h2>SUVs</h2>
+          <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+          <a href="https://www.webmotors.com.br/SUVs" target="_blank">ver mais</a>
+        </div>
+        <div>
+          <img src={luxury} alt="" />
+          <h2>Luxuosos</h2>
+          <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+          <a href="https://www.webmotors.com.br/carros-de-luxo/carros/rs-porto-alegre?estadocidade=Rio%20Grande%20do%20Sul%20-%20Porto%20Alegre&necessidade=Carros%20de%20luxo&idcmpint=t1:c17:m07:busque-por-categorias::carros-de-luxo:comprar" target="_blank">ver mais</a>
+        </div>
+
+
+      </section>
+    </main>
   )
 }
