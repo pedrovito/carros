125e0aee6b522a8233fb28ff424b8da96126d12a
pedrovito <pedro.v.oliveira007@gmail.com>
2024-03-19 01:06:47 -0300

    pedrinho

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Suvs from "./components/Suvs";
 export default function App(){
   return(
     <main className="min-h-svh bg-offwhitebg py-20 px-6 lg:flex justify-center items-center">
-      <div className="lg:flex mx-w-[920px]">
+      <div className="lg:flex max-w-[920px]">
         
         <Sedans/>
         <Suvs/>
